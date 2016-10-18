faa6de63ae6028fd56885f51ced0fa697aa627e9
chups2themax <osuna.9@gmail.com>
2016-10-17 23:56:04 -0700

    Fixed Error with FCFS and SSTF algorithms. Both Working.

--- lab1.py
+++ lab1.py
@@ -4,8 +4,8 @@
 # c. SCAN - scan to zero and then back up - given start head location
 # d. C-SCAN - scan to end of disk then back down - given start head location
 # e. LOOK - 
-# f. C-LOOK
-#  Your program will service a disk with 5,000 cylinders numbered 0 to 4,999. The program will generate a random 
+# f. C-LOOK - 
+# Your program will service a disk with 5,000 cylinders numbered 0 to 4,999. The program will generate a random 
 # series of 1,000 cylinder requests and service them according to each of the algorithms listed above. The 
 # program will be passed the initial position of the disk head (as a parameter on the command line) and report 
 # the total amount of head movement required by each algorithm.
@@ -19,12 +19,12 @@ import numpy
 i = 0
 cylinder = 4999
 block = 512
-request = []
+SIZE = 10
 
-testArry = [98,183,37,122,14,128,65,67]
+testArry = [98,183,37,122,14,128,65,67] 
 
 #print out the average head movement for each algorithm, store each run in a temp
-FSCSheadMov = []
+FCFSheadMov = []
 SSTFheadMov = []
 
 
@@ -33,58 +33,43 @@ startHead = int(startHead)
 
 #generates several request for disk access
 def getSchedule():
-	request.append(startHead)
-	while(len(request) < 10):
+	request = []
+	#request.append(startHead)
+	while(len(request) < SIZE):
 		request.append(random.randint(0,cylinder))
 	return request
 
 #First Come First Serve Algorithm 
 def FCFS(scheduling):
+	minDistArry = []
 	j = 1
-	for access in scheduling:
-		if( j <= len(scheduling)):
-			headMov = abs(access - (scheduling[j]))
-			FSCSheadMov.append(headMov)
-			j += 1
+	scheduling.insert(0, startHead)
+	for x in xrange(len(scheduling) - 1):
+			headMov = abs( scheduling[x] - scheduling[x+1] )
+			minDistArry.append(headMov)
+
+	FCFSheadMov.append(numpy.mean(minDistArry))
 
 #Shortest Seek Time First Algorithm
-minDistArry = []
 def SSTF(scheduling, headPos):
-	
-	if(len(minDistArry) > 99):
-		minDistArry = []
+	minDistArry = []
+	while(len(scheduling) > 1):
+		minDist = 0
+		if(len(scheduling) > 1):
+			minDist = abs(scheduling[0] - headPos)	
+			arrElmt = scheduling[0]
+			
+			for x in scheduling:
+				if( abs(headPos - x) < minDist):
+					minDist = abs(headPos - x)
+					arrElmt = x
 
-	minDist = 0
-	if(len(scheduling) > 1):
-		minDist = abs(scheduling[0] - headPos)	
-		arrElmt = scheduling[0]
-		
-		for x in scheduling:
-			#print "in position: " + str(x)
-			if( abs(headPos - x) < minDist):
-				minDist = abs(headPos - x)
-				arrElmt = x
-
-		print arrElmt
-		print "size of array: " + str(len(scheduling))
-		minDistArry.append(minDist)
-		scheduling.remove(arrElmt)
-		SSTF(scheduling, arrElmt)
+			minDistArry.append(minDist)
+			scheduling.remove(arrElmt)
 
 	SSTFheadMov.append(numpy.mean(minDistArry))
 
+FCFS(testArry)
 SSTF(testArry, startHead)
-
-''' main 
-while(i < 10):
-	request = getSchedule
-	print("request " + str(i) + " made")
-	print(request)
-	print("head movement " + str(i) + " between each request")
-	print(headMovement)
-	print("")
-	request = []
-	headMovement = []
-	i += 1
-'''
-
+print FCFSheadMov
+print SSTFheadMov
