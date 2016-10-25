7cd6b1638ea46f555fb89c01a48817be3c74b88c
chups2themax <osuna.9@gmail.com>
2016-10-24 19:54:39 -0700

    Updated README file for lab 1 specifications and how to turn in

--- lab1.py
+++ lab1.py
@@ -1,22 +1,10 @@
-# Write a program that implements the following disk-scheduling algorithms:
-# a.X FCFS -fifo
-# b.X SSTF - sortest path to next head movement
-# c.X SCAN - scan to zero and then back up - given start head location
-# d.X C-SCAN - scan to end of disk then back down - given start head location
-# e.X LOOK - 
-# f.X C-LOOK - 
-# Your program will service a disk with 5,000 cylinders numbered 0 to 4,999. The program will generate a random 
-# series of 1,000 cylinder requests and service them according to each of the algorithms listed above. The 
-# program will be passed the initial position of the disk head (as a parameter on the command line) and report 
-# the total amount of head movement required by each algorithm.
-
-#HOW TO TURN IN
-#source code, screenshot of executed code, results, strategy, 
+#Adrian Osuna
+#CSE 461 - Dr. Gomez
+#Lab 1
 
 import random
 import numpy
 
-i = 0
 cylinder = 4999
 block = 512
 SIZE = 10
