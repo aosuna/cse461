981ff7d4f1add9b3480a3d553f8f0f009cb3612d
chups2themax <osuna.9@gmail.com>
2016-11-10 19:35:03 -0800

    Added Hash table and search algorithms

--- lab3.py
+++ lab3.py
@@ -1,11 +1,14 @@
 #Adrian Osuna
-#CSE 461 Lab 3 
-
+#CSE 461 - Dr. Gomez
+#Lab 3
+# -*- coding: utf-8 -*-
 import string
 import random
 import time
+import numpy
 
-SIZE = 10
+SIZE = 10000
+sizeHash = SIZE * 2
 
 def string_generator():
 	files = []
@@ -17,12 +20,29 @@ def string_generator():
 
 def search_unsorted(listArry, check):
 	start = time.time()
-	if check in listArry:
-		print ("Your input is in the array!")
-	else:
-		print ("Your input is not in the array.")
+	for x in xrange(len(listArry)):
+		if listArry[x] == check:
+			#print ("Your input is in the array!"), x
+			break
 	stop = time.time()
 	return (stop - start)
+
+def hashIt(listArry):
+	hash_table = [[] for x in xrange(sizeHash)]
+	for x in xrange(len(listArry)):
+		hashVal = hash(listArry[x]) % sizeHash
+		hash_table[hashVal].append(listArry[x])
+	return hash_table
+
+def search_hash(hashtable, check):
+	search = hash(check) % sizeHash
+	start = time.time()
+	for x in hashtable[search]:
+		if x == check:
+			#print("Your input is in the array!")
+			break
+	stop = time.time()
+	return(stop - start)
 
 def search_sorted(listArry, check):
 	start = time.time()
@@ -34,32 +54,33 @@ def search_sorted(listArry, check):
 			high = mid - 1
 		elif(check > listArry[mid]):
 			if low == mid:
+				#print("Your input was not in the array.")
 				break
 			low = mid + 1
 		elif(check == listArry[mid]):
-			print ("Your input is in the array at index: "), mid
+			#print ("Your input is in the array at index: "), mid
 			break
-		else:
-			print("Your input was not in the array.")
 	stop = time.time()
 	return (stop - start)
 
 if __name__ == '__main__':
-	inputChk = raw_input("Enter a string of 5 characters long: ")
-	if(len(inputChk) != 5):
-		print ("Input was invalid, please enter 5 characters")
-	else:
+	unsort = []
+	binsearch = []
+	hashlook = []
+	for x in xrange(100):
 		stringInput = string_generator()
+		inputChk = stringInput[random.randint(0,SIZE-1)]
 		inputChk = inputChk.upper()
-
+		#print inputChk
 		unsortSearch = search_unsorted(stringInput, inputChk)
-		print ("Time to search a unsorted list: "),unsortSearch * 1000
+		unsort.append(unsortSearch * 1000)
+		sortInput = sorted(stringInput)
+		sortedSearch = search_sorted(sortInput, inputChk)
+		binsearch.append(sortedSearch * 1000)
+		hashSearch = search_hash(hashIt(stringInput), inputChk)
+		hashlook.append(hashSearch * 1000)
 
-		stringInput = sorted(stringInput)
-
-		sortedSearch = search_sorted(stringInput, inputChk)
-		print ("Time to search a sorted list: "),sortedSearch * 1000
-
-
-
+	print("Average for 100 list; unsorted list : "), numpy.mean(unsort)
+	print("Average for 100 list; binary search : "), numpy.mean(binsearch)
+	print("Average for 100 list; hash look up  : "), numpy.mean(hashlook)
 
