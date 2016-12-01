d2bb223f26c12f7ca34e38010fbd367f90d295d2
Adrian Osuna <osuna.9@gmail.com>
2016-11-30 18:00:04 -0800

    Working Lab 4

--- lab4.cpp
+++ lab4.cpp
@@ -1,43 +1,35 @@
-#include "timer.c"
-#include <vector>
 #include <iostream>
-#include <sys/time.h>
-
+#include "timer.c"
 //how to compile --> g++ -O3 -o time lab4.cpp
-
 //run sysctl -a | grep cache to see cache size avaiable on MAC OS. 
 using namespace std;
 
 int main(){
-	//create 2 arrays
-	//size of array needs to be bigger than CACHE
-	long size = 2000000000; // set array size to 5K
-	double A[size];
+	long size = 5e8; //size of array needs to be bigger than CACHE
+	double A[size];  //create 2 arrays
 	double B[size];
-
-	//init A
-	srand (time(NULL));
-	for(int i = 0; i < size; i++){
-		A[i] = rand()%size;
-	}
-	
-	double X = milisecond_timer();
 	double Y = 0;
 
+	//init A
+	srand(time(NULL));
+	for(int i = 0; i < size; i++){
+		A[i] = rand()%(10000);
+	}
+	double X = milisecond_timer();
 	//copy A -> B
 	for (int i = 0; i < size; i++){
 		B[i] = A[i];
-		Y += milisecond_timer() - X;
+		if(i == size-1)
+			Y = milisecond_timer();
 	}
+	double time = (Y - X);
+	double bandwidth = (size) / (time/1000);
 	
-	double time = Y - X;
-	double bandwidth = size / time;
+	printf("\nX value = %f ms\n", X);
+	printf("Y value = %f ms\n", Y);
 
-	printf("\nX value = %f\n", X);
-	printf("Y value = %f\n", Y);
-
-	printf("Time to execute is %f\n", time);
-	printf("bandwidth: %f\n", bandwidth);
+	printf("Time to execute is %f ms\n", time);
+	printf("bandwidth: %f bytes/s\n", bandwidth);
 
 	return 0;
 }
